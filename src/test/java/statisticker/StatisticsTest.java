d565b433c97a90db151d3acb3c40e18ca2f012da
Shruthi-Murthy <77479146+Shruthi-Murthy@users.noreply.github.com>
2021-01-28 17:37:30 +0530

    Update StatisticsTest.java

--- src/test/java/statisticker/StatisticsTest.java
+++ src/test/java/statisticker/StatisticsTest.java
@@ -15,26 +15,24 @@ public class StatisticsTest
     public void reportsAverageMinMaxx()
     {
         Float[] numbers = {1.5f, 8.9f, 3.2f, 4.5f};
-        List<___> numberList = Arrays.asList(numbers);
+        List<Float> numberList = Arrays.asList(numbers);
 
         Statistics.Stats s = Statistics.getStatistics(numberList);
 
         float epsilon = 0.001f;
-        assertEquals(s.average, 4.525f, epsilon);
-        assertEquals(s.min, 1.5f, epsilon);
-        assertEquals(s.max, 8.9f, epsilon);
+        assertEquals(s.average(), 4.525f, epsilon);
+        assertEquals(s.min(), 1.5f, epsilon);
+        assertEquals(s.max(), 8.9f, epsilon);
     }
     @Test
     public void reportsNaNForEmptyInput()
     {
-        List<___> emptyList = new ArrayList<___>();
+        List<Float> emptyList = new ArrayList<>();
 
         Statistics.Stats s = Statistics.getStatistics(emptyList);
-
-        //All fields of computedStats (average, max, min) must be
-        //Float.NaN (not-a-number), as described in
-        //https://www.geeksforgeeks.org/nan-not-number-java/
-        //Design the asserts here and implement accordingly.
+        asssetTrue(Float.isNAN(s.average()));
+        asssetTrue(Float.isNAN(Float.isNAN(s.min()));
+        asssetTrue(Float.isNAN(s.max()));
     }
     @Test
     public void reportsAlertsIfMaxIsMoreThanThreshold()
